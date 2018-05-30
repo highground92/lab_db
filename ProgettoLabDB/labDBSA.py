7c0c32bbadc845d5174fc4c6b39fbb60c3b4df1c
Fabio <fabiosss@hotmail.it>
2018-05-30 15:21:50 +0200

    riconoscimento emoji
    primo riconoscimento di emoji, dubbio sull'encoding che non rispecchia la lista data su ilearn

--- ProgettoLabDB/labDBSA.py
+++ ProgettoLabDB/labDBSA.py
@@ -4,9 +4,26 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
 import os
+
+
+# -*- encoding: utf-8 -*-
+# pip install emoji
+
+import emoji
+
+
+def char_is_emoji(character):
+    return character in emoji.UNICODE_EMOJI
+
+
+def text_has_emoji(text):
+    for character in text:
+        if character in emoji.UNICODE_EMOJI:
+            return True
+    return False
  
 #Leggo il dataSet e lo metto come stringa
-with open(os.path.abspath("dataSet/dataset_joy_piccolo.txt"), 'r') as myfile:
+with open(os.path.abspath("dataSet/dataset_joy_piccolo.txt"), 'r', encoding='utf-8') as myfile:
     data=myfile.read().replace('\n', '') 
 
 #Levo le stopWords, TODO mettere le stopWords della prof
@@ -17,7 +34,7 @@ wordsFiltered = [] #File filtrato
 for w in words:
     if w not in stopWords:
         wordsFiltered.append(w)
-        
+
 #Levo USERNAME e URL
 deleteWords = ['USERNAME', 'URL']
 for word in list(wordsFiltered):  
@@ -26,7 +43,7 @@ for word in list(wordsFiltered):
 
 #Processo hashtags.
 hashtags = []
-    #Metodo precedente quando ogni elemento era tokenizzato come parola
+#Metodo precedente quando ogni elemento era tokenizzato come parola
 """hashSymbol= ['#']
 for index, h in enumerate(list(wordsFiltered)):
     if h in hashSymbol:
@@ -41,6 +58,8 @@ for h in wordsFiltered:
         print("H inizia con "+h)            
         hashtags.append(h)
         wordsFiltered.remove(h)
+        
+
 
 
             
@@ -51,3 +70,12 @@ print(hashtags)
 print("TWEET MODIFICATI: ")    
 print(wordsFiltered)
 #print(sentenceFiltered)
+
+#lista delle emoji con nome (es :flushed_face:) e codice utf8
+print('Lista Emoji tradotte')
+for w in wordsFiltered:
+    if text_has_emoji(w):
+        print(emoji.demojize(w))
+        print(w.encode(encoding='utf-8'))
+        
+        
