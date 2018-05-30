2c940a65f688478886a014965baf842488fab6a5
Stefano <stevi13@hotmail.it>
2018-05-30 15:06:41 +0200

    emoji fatte
    da capire perché non legge le emoji di fila (stessa cosa per gli hashtags).
    Ho rifatto i file dei data e degli Emoji, quelli di prima forse non vanno, sostituiteli senza cabiare una virgola

--- ProgettoLabDB/labDBSA.py
+++ ProgettoLabDB/labDBSA.py
@@ -4,6 +4,7 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
 import os
+import emoji
  
 #Leggo il dataSet e lo metto come stringa
 with open(os.path.abspath("dataSet/dataset_joy_piccolo.txt"), 'r') as myfile:
@@ -13,7 +14,6 @@ with open(os.path.abspath("dataSet/dataset_joy_piccolo.txt"), 'r') as myfile:
 stopWords = set(stopwords.words('english'))
 words = TweetTokenizer().tokenize(data)
 wordsFiltered = [] #File filtrato
-
 for w in words:
     if w not in stopWords:
         wordsFiltered.append(w)
@@ -26,23 +26,54 @@ for word in list(wordsFiltered):
 
 #Processo hashtags.
 hashtags = []
-    #Metodo precedente quando ogni elemento era tokenizzato come parola
-"""hashSymbol= ['#']
-for index, h in enumerate(list(wordsFiltered)):
-    if h in hashSymbol:
-        nxt=wordsFiltered.index(h)
-        value=wordsFiltered[nxt+1]
-        hashtags.append(value)
-        wordsFiltered.remove(h)
-        wordsFiltered.remove(value)"""
-#for index, h in enumerate(list(wordsFiltered)):
 for h in wordsFiltered:
     if h.startswith("#"):
-        print("H inizia con "+h)            
         hashtags.append(h)
         wordsFiltered.remove(h)
+        h=0
 
+#Emoji. TODO  capire perché non leva tutto 
+print("EMOJI POS:")
+print(emoji.EmojiPos)
+countEmoPos = 0
+countEmoNeg = 0
+countEmoNeut = 0
+for h in wordsFiltered:
+    for j in emoji.EmojiPos:
+        if h==j:
+            countEmoPos += 1
+            wordsFiltered.remove(h)
+    for k in emoji.EmojiNeg:
+        if h==k:
+            countEmoNeg += 1
+            wordsFiltered.remove(h)
+    for l in emoji.OthersEmoji:
+        if h==l:
+            countEmoNeut += 1
+            wordsFiltered.remove(h)
 
+#Emoticons
+countPosEmoticons = 0
+countNegEmoticons = 0
+for h in wordsFiltered:
+    for j in emoji.posemoticons:
+        if h == j:
+            countPosEmoticons +=1
+            wordsFiltered.remove(h)
+    for k in emoji.negemoticons:
+        if h == k:
+            countNegEmoticons +=1
+            wordsFiltered.remove(h)
+
+#print("FACCIA CHE RIDE LINGUA FUORI")
+print(emoji.EmojiPos[20]==wordsFiltered[18])
+#print(wordsFiltered[18])
+print("STAMPO VALORI: ")
+print("countEmoPos: "+repr(countEmoPos))
+print("countEmoNeg: "+repr(countEmoNeg))
+print("countEmoNeut: "+repr(countEmoNeut))
+print("countPosEmoticons: "+repr(countPosEmoticons))
+print("countNegEmoticons: "+repr(countNegEmoticons))
             
 print("LISTA HASHTAGS: ")            
 print(hashtags)    
