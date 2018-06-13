5bbb8c6a2ad78cacb4ec900a5299a3421f22dec3
Fabio <fabiosss@hotmail.it>
2018-06-13 14:25:52 +0200

    Corretto processamento delle parole
    Ora in words_dict per le risorse appartenenti a con-score viene visualizzato il punteggio al posto di 1 o 0

--- ProgettoLabDB/labDBSA.py
+++ ProgettoLabDB/labDBSA.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from nltk.stem import PorterStemmer 
 from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
@@ -8,14 +7,6 @@ from resources import emoji_list, punctuation_mark, slang_words, pos
 import collections
 import glob
 import csv
-<<<<<<< HEAD
-import string
-
-# -*- encoding: utf-8 -*-
-# pip install emoji
-
-=======
->>>>>>> Stefano
 import emoji
 import os
 
@@ -38,8 +29,11 @@ def process_dataSets(wordsFiltered,lexical_resources):
             presence_count = 0
             for res_name in lexical_resources[l]:
                 if w in lexical_resources[l][res_name]:
-                    resources_dict[res_name] = 1
-                    presence_count += 1
+                    if l == 'con-score':
+                        resources_dict[res_name] = lexical_resources[l][res_name][w]
+                    else:
+                        resources_dict[res_name] = 1
+                        presence_count += 1
                 else:
                     resources_dict[res_name] = 0
             resources_dict['lexical_res_presence'] = presence_count
@@ -90,124 +84,6 @@ def get_lexical_resources():
     os.chdir(owd)
     return lexical_dictionary
  
-<<<<<<< HEAD
-#Leggo il dataSet e lo metto come stringa
-with open(os.path.abspath("dataSet/dataset_joy_piccolo.txt"), 'r', encoding='utf-8') as myfile:
-    data=myfile.read().replace('\n', '') 
-
-#Levo le stopWords, TODO mettere le stopWords della prof, spostare questo piú avanti
-stopWords = set(stopwords.words('english'))
-words = TweetTokenizer().tokenize(data)
-wordsFiltered = [] #File filtrato
-for w in words:
-    if w not in stopWords:
-        wordsFiltered.append(w)
-
-#Levo USERNAME e URL
-deleteWords = ['USERNAME', 'URL']
-wordsFiltered = [h for h in wordsFiltered if h not in deleteWords]
-
-#Processo hashtags.
-hashtags = []
-#Metodo precedente quando ogni elemento era tokenizzato come parola
-"""hashSymbol= ['#']
-for index, h in enumerate(list(wordsFiltered)):
-    if h in hashSymbol:
-        nxt=wordsFiltered.index(h)
-        value=wordsFiltered[nxt+1]
-        hashtags.append(value)
-        wordsFiltered.remove(h)
-        wordsFiltered.remove(value)"""
-#for index, h in enumerate(list(wordsFiltered)):
-for h in wordsFiltered:
-    if h.startswith("#"):
-        #print("H inizia con "+h)            
-        hashtags.append(h)
-        wordsFiltered.remove(h)
-        
-hashtags=[h for h in wordsFiltered if h.startswith("#")]
-wordsFiltered=[h for h in wordsFiltered if not h.startswith("#")]
-                                                            
-#Emoji.             
-countEmoPos=len([h for h in wordsFiltered if h in set(emoji_list.EmojiPos)])
-wordsFiltered=[h for h in wordsFiltered if h not in set(emoji_list.EmojiPos)]
-
-countEmoNeg=len([h for h in wordsFiltered if h in set(emoji_list.EmojiNeg)])
-wordsFiltered=[h for h in wordsFiltered if h not in set(emoji_list.EmojiNeg)]
-
-countEmoNeut=len([h for h in wordsFiltered if h in set(emoji_list.OthersEmoji)])
-wordsFiltered=[h for h in wordsFiltered if h not in set(emoji_list.OthersEmoji)]
-
-#Emoticons
-countPosEmoticons=len([h for h in wordsFiltered if h in set(emoji_list.posemoticons)])
-wordsFiltered=[h for h in wordsFiltered if h not in set(emoji_list.posemoticons)]
-
-countNegEmoticons=len([h for h in wordsFiltered if h in set(emoji_list.negemoticons)])
-wordsFiltered=[h for h in wordsFiltered if h not in set(emoji_list.negemoticons)]
-
-
-print("STAMPO VALORI: ")
-print("countEmoPos: "+repr(countEmoPos))
-print("countEmoNeg: "+repr(countEmoNeg))
-print("countEmoNeut: "+repr(countEmoNeut))
-print("countPosEmoticons: "+repr(countPosEmoticons))
-print("countNegEmoticons: "+repr(countNegEmoticons))
-
-#lista delle emoji con nome (es :flushed_face:) e codice utf8
-"""
-print('Lista Emoji tradotte')
-for w in wordsFiltered:
-    if text_has_emoji(w):
-        print(emoji.demojize(w))
-        print(w.encode(encoding='utf-8'))
-        #print(w.decode('unicode-escape').encode('latin1').decode('utf8'))
-"""
-
-#leggo i nomi dei sentimenti e delle risorse
-lexical_resources = get_lexical_resources()
-
-words_dict = process_dataSets(wordsFiltered,lexical_resources)
-print('//////////////')
-print('//////////////')
-#with open('words_dict.txt', 'w') as file:
-#     file.write(json.dumps(words_dict))
-print('LEXICAL RESOURCES:')
-for w in lexical_resources:
-    print('sentiment:' + w)
-    for r in lexical_resources[w]:
-        print(r)
-
-"""
-from pymongo import MongoClient
-
-# Create connection to MongoDB
-client = MongoClient('localhost', 27017)
-db = client['database_di_prova']
-collection = db['words_dict_prova']
-
-# Build a basic dictionary
-
-# Insert the dictionary into Mongo
-collection.insert(words_dict)
-print('done')
-"""
-
-import cx_Oracle
-
-con = cx_Oracle.connect('labdb1718/root@127.0.0.1/xe')
-cur = con.cursor()
-for word in words_dict:
-    query = "insert into JOY values ('"+word+"',"+repr(words_dict[word]["joy"]["EmoSN_joy"])+","+repr(words_dict[word]["joy"]["NRC_joy"])+","+repr(words_dict[word]["joy"]["sentisense_joy"])+","+repr(words_dict[word]["joy"]["lexical_res_presence"])+","+repr(words_dict[word]["joy"]["lexical_res_frequency"])+")"
-    print(query)
-    try:
-        cur.execute(query)
-        con.commit()
-    except:
-        print('rollback')
-        con.rollback()
-cur.close()
-con.close()
-=======
 #Metodo principale di trattamento dei tweets
 def run_clean_tweet(data, parentDir):
     
@@ -271,7 +147,7 @@ def run_clean_tweet(data, parentDir):
     dictionaryWordsCount= collections.Counter(wordsFiltered)
     
     
-    print(wordsFiltered)
+    #print(wordsFiltered)
     """
     print("STAMPO VALORI: ")
     print("countEmoPos: "+repr(countEmoPos))
@@ -298,11 +174,12 @@ def run_clean_tweet(data, parentDir):
     lexical_resources = get_lexical_resources()
     
     words_dict = process_dataSets(wordsFiltered,lexical_resources)
-    
+    return words_dict
+    """
     print('//////////////')
     print('//////////////')# TODO: cambiare il nome del dizionario in base al dataset utilizzato
     with open('words_dict.txt', 'w') as file:
-         file.write(json.dumps(words_dict))
+         file.write(json.dumps(words_dict)) """
     """     
     print('LEXICAL RESOURCES:')
     for w in lexical_resources:
@@ -311,4 +188,3 @@ def run_clean_tweet(data, parentDir):
             print(r)"""
 
         
->>>>>>> Stefano
--- ProgettoLabDB/main.py
+++ ProgettoLabDB/main.py
@@ -2,7 +2,7 @@
 import os
 import glob
 import labDBSA
-
+import cx_Oracle
 def get_all_dataSet():
     owd= os.getcwd()
     os.chdir(owd+"/dataSet/")
@@ -14,10 +14,27 @@ def get_all_dataSet():
 dataset_list=get_all_dataSet()
 owd= os.getcwd()
 parentDir=os.path.abspath(os.path.join(owd, os.pardir))
+print(parentDir)
 for file in dataset_list :    
     with open(file, 'r', encoding='utf-8') as myfile:
         print(file)
         data=myfile.read().replace('\n', '')
-        labDBSA.run_clean_tweet(data,parentDir)
+        words_dict = labDBSA.run_clean_tweet(data,parentDir)
         owd= os.getcwd()
         os.chdir(owd+"/dataSet/")
+        
+"""
+con = cx_Oracle.connect('labdb1718/root@127.0.0.1/xe')
+cur = con.cursor()
+for word in words_dict:
+    query = "insert into JOY values ('"+word+"',"+repr(words_dict[word]["joy"]["EmoSN_joy"])+","+repr(words_dict[word]["joy"]["NRC_joy"])+","+repr(words_dict[word]["joy"]["sentisense_joy"])+","+repr(words_dict[word]["joy"]["lexical_res_presence"])+","+repr(words_dict[word]["joy"]["lexical_res_frequency"])+")"
+    print(query)
+    try:
+        cur.execute(query)
+        con.commit()
+    except:
+        print('rollback')
+        con.rollback()
+cur.close()
+con.close()
+"""
