9c2e1f856bb14df8eac33e23551f8246bcaf90e4
Fabio <fabiosss@hotmail.it>
2018-06-15 17:02:29 +0200

    Mongo fatto
    Da completare ancora la parte Oracle.
    Fatto inserimento e mapreduce di un dataset su mongo

--- ProgettoLabDB/main.py
+++ ProgettoLabDB/main.py
@@ -3,6 +3,16 @@ import os
 import glob
 import labDBSA
 import oracleDB
+from pymongo import MongoClient
+from collections import defaultdict
+
+# Create a function called "chunks" with two arguments, l and n:
+def chunks(l, n):
+    # For item i in a range that is a length of l,
+    for i in range(0, len(l), n):
+        # Create an index range for l of n items:
+        yield l[i:i+n]
+
 
 def get_all_dataSet():
     owd= os.getcwd()
@@ -16,13 +26,31 @@ dataset_list=get_all_dataSet()
 owd= os.getcwd()
 parentDir=os.path.abspath(os.path.join(owd, os.pardir))
 print(parentDir)
+client = MongoClient('localhost', 27017)
+db = client['dbTweetProva']
+collection = db['tweet_dict']
+count = 0
 for file in dataset_list :    
     with open(file, 'r', encoding='utf-8') as myfile:
-        print(file)
         data=myfile.read().replace('\n', '')
         wordsFiltered = labDBSA.run_clean_tweet(data,parentDir)
         words_dict = labDBSA.createDictionary(wordsFiltered)
         owd= os.getcwd()
+        for w in wordsFiltered:
+            mongo_word = defaultdict()
+            mongo_word['sentiment'] = file[:-4]
+            mongo_word['_id'] = count
+            mongo_word['word'] = w
+            count = count+1
+            collection.insert_one(mongo_word)
         os.chdir(owd+"/dataSet/")
+#    mongo_words[file[:-4]] = wordsFiltered
+    
+"""
+client = MongoClient('localhost', 27017)
+db = client['dbTweetProva']
+collection = db['tweet_dict']
+collection.insert_one(mongo_words)
 
-oracleDB.connessioneOracle(words_dict, wordsFiltered)    
+#oracleDB.connessioneOracle(words_dict, wordsFiltered)    
+"""
---
+++ ProgettoLabDB/mapReduce.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 15 16:43:06 2018
+
+@author: Fabio
+"""
+from bson.code import Code
+from pymongo import MongoClient
+
+
+client = MongoClient('localhost', 27017)
+db = client['dbTweetProva']
+collection = db['tweet_dict']
+
+
+map = Code("function() {emit(this.word,1);};")
+reduce = Code("function(word,count) {return Array.sum(count);};")
+result = collection.map_reduce(map, reduce, "words_frequency")
+
+client.close()
